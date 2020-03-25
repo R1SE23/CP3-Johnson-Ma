79584acddf1d8b8448827610773308f0bfdefcde
R1SE23 <R1SE23@users.noreply.github.com>
2020-03-25 20:50:01 +0700

    Create Exercise4_asd_asd.py

---
+++ Exercise4_asd_asd.py
@@ -0,0 +1,18 @@
+A = 20.35
+B = 25.22
+C = 22.22
+D = 23.56
+
+
+
+
+print('--- Your Score ---')
+
+
+print('Foundation English:', A, 'Point')
+
+print('General Business:', B, 'Point')
+
+print('Introduction to Computer Systems:' , C, 'Point')
+
+print('Computer Programming:' , D, 'Point')
