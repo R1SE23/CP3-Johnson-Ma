e98c2ceb4ac7c1cacb476d67361f7318fff90068
R1SE23 <60549326+R1SE23@users.noreply.github.com>
2020-03-25 22:58:19 +0700

    Add files via upload

---
+++ Assignment/Exercise.5.2_Johnson_Ma.py
@@ -0,0 +1,11 @@
+
+print('---------------')
+print('Simple Physics')
+print('---------------')
+
+
+s = int(input("distance/displacement(km) is = "))
+t = int(input("total time is(hr) ="))
+v = s/t
+
+print("speed/velocity is =", v, "km/hr")
