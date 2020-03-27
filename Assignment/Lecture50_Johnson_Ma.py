9c46e798e2b16404e2ba1c9a74d5247fde92fe59
R1SE23 <R1SE23@users.noreply.github.com>
2020-03-27 23:25:47 +0700

    Create Lecture50_Johnson_Ma.py

---
+++ Assignment/Lecture50_Johnson_Ma.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(10,5)
+def subtractNumber(x,y):
+    print(x-y)
+subtractNumber(10,5)
+def multiplyNumber(x,y):
+    print(x*y)
+multiplyNumber(10,5)
+def devideNumber(x,y):
+    print(x/y)
+devideNumber(10,5)
+
