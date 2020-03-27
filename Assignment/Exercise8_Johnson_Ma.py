925b720db501d4c830f6ea605d4ba0882e2f8801
R1SE23 <60549326+R1SE23@users.noreply.github.com>
2020-03-27 22:53:53 +0700

    Add files via upload

--- Assignment/Exercise8_Johnson_Ma.py
+++ Assignment/Exercise8_Johnson_Ma.py
@@ -1,45 +1,39 @@
-print('♝♝♝♝♝♝♝♝♝♝♝')
-print("Welcome to EShop!!!")
-print('♝♝♝♝♝♝♝♝♝♝♝')
+price = {"Sprite": 1.50, "Tea": 2.00, "Coffee": 1.00, "Water": 0.50}
+shopping_basket = {}
 x = 'qwerqwer'
 y = '123123'
 while input("Username: ") != x or input("Password: ") != y:
     print("Incorrect Username/Password!!!")
-print('You have one option to select products from any section')
-print('1.Book Section')
-print('2.Drink Section')
+print('♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝')
+print("Welcome to the online drink store!\nThese are the drinks we offer\n1. Sprite: $1.50\n2. Tea: $2.00\n3. Coffee: $1.00\n4. Water: $0.50")
+print('♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝♝')
+buy_another = 1
+total_cost = 0
+total = 0
 
-orderSelect = int(input('>> '))
-if orderSelect == 1:
-    print('1.High Speed Reading techniques by Pane Edin : 250 B')
-    print('2.How to be productive by Josh White : 300 B')
-    amount = int(input('>> '))
-    if amount == 1:
-        print('How many books do you want?')
-        numofbook = int(input())
-        price = 250 * numofbook
-        print(price)
+while buy_another != 0:
+    option = int(input("Which drink would you like to purchase?: "))
 
-    elif amount == 2:
-        print('How many books do you want?')
-        numofbook = int(input())
-        price = 300 * numofbook
-        print(price)
+    if option == 1:
+        qnty = int(input("Enter the quantity: "))
+        total = qnty * 1.50
+        print("The price is: " + str(total))
+    elif option == 2:
+        qnty = int(input("Enter the quantity: "))
+        total = qnty * 2.00
+        print("The price is: " + str(total))
+    elif option == 3:
+        qnty = int(input("Enter the quantity: "))
+        total = qnty * 1.00
+        print("The price is: " + str(total))
+    elif option == 4:
+        qnty = int(input("Enter the quantity: "))
+        total = qnty * 0.50
+        print("The price is: " + str(total))
+    else:
+        print('Please select again!!!')
 
-elif orderSelect == 2:
-    print('1.Pepsi : 20 B')
-    print('2.Coke : 15 B')
-    quan = int(input('>> '))
-    if quan == 1:
-        print('How many Pepsi do you want?')
-        pep = int(input())
-        price = 20 * pep
-        print(price)
+    total_cost += total
 
-    elif quan == 2:
-
-        print('How many Coke do you want?')
-        coke = int(input())
-        price = 15 * coke
-        print(price)
-
+    buy_another = int(input("Would you like another item? enter Yes(1) or No(0):"))
+print("The total price of your basket is: ", total_cost)
