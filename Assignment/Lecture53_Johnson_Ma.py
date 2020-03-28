a706357a32026e4ed15418491fe7186c32fcb80e
R1SE23 <R1SE23@users.noreply.github.com>
2020-03-28 14:10:36 +0700

    updated

--- Assignment/Lecture53_Johnson_Ma.py
+++ Assignment/Lecture53_Johnson_Ma.py
@@ -2,4 +2,4 @@ def vatCalculate(totalprice):
     result = totalprice+(totalprice*7/100)
     return result
 totalprice = float(input('Price: '))
-print(vatCalculate(totalprice))
+print(vatCalculate(totalprice))sss
